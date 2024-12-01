3395fc3bc79f0048b6d7c0df6943e36b1e7c1b34
Ankerrrr <80147818+Ankerrrr@users.noreply.github.com>
2024-12-01 17:38:53 +0800

    addComment

--- UVA10062/main.cpp
+++ UVA10062/main.cpp
@@ -5,14 +5,14 @@
 
 using namespace std;
 
-bool comp(pair<int, int> a, pair<int, int> b) {
+bool comp(pair<int, int> a, pair<int, int> b) { // 自訂 sort 條件
   if (a.second == b.second) {
     return a.first > b.first;
   }
   return a.second < b.second;
 }
 
-void inp(vector<pair<int, int>> &table, bool &end) {
+void inp(vector<pair<int, int>> &table, bool &end) { // 傳 vector
   string inp;
   getline(cin, inp);
 
@@ -31,7 +31,7 @@ void printOut(vector<pair<int, int>> &table, bool &firstLine) {
   } else {
     firstLine = false;
   }
-  sort(table.begin(), table.end(), comp);
+  sort(table.begin(), table.end(), comp); // 最右邊放排序方式 greather<int>()大到小 ,預設小到大
   for (int i = 0; i < table.size(); i++) {
     if (table[i].second > 0) {
       cout << (int)table[i].first << " " << table[i].second << endl;
@@ -44,8 +44,8 @@ int main() {
   while (1) {
     bool end = false;
 
-    vector<pair<int, int>> table(128);
-    for (int i = 0; i < 128; i++) {
+    vector<pair<int, int>> table(128); // pair底下包含 first, second
+    for (int i = 0; i < 128; i++) {    // pair不能使用 (size, value)的方式初始所有值
       table[i].first = i;
       table[i].second = 0;
     }
